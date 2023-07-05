c8adea8513d6c966bfaa0db688f8eb62582ea112
kinoppol <kinoppol@hotmail.com>
2023-07-05 15:09:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,6 @@ header('Content-Type: text/html; charset=utf-8');
 //print_r($_SESSION);
 if(empty($_SESSION['userid'])){
     echo '<title>RMS-Authentication</title><meta http-equiv="Content-type" content="text/html; charset=utf-8" />คุณยังไม่ได้เข้าสู่ระบบ โปรดรอสักครู่..<meta http-equiv="refresh" content="2;url=../">';
+}else{
+    echo '<title>RMS-Authentication</title><meta http-equiv="Content-type" content="text/html; charset=utf-8" />โปรดรอสักครู่..<meta http-equiv="refresh" content="2;url=./authen.php">';
 }
