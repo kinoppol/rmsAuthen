b4b213a4847e590e8cccb688ebcdbf2b073041b3
kinoppol <kinoppol@hotmail.com>
2023-07-05 14:56:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
 ob_start();
 session_start();
-print_r($_SESSION);
+if(empty($_SESSION['userid'])){
+    echo 'คุณยังไม่ได้เข้าสู่ระบบ โปรดรอสักครู่<meta http-equiv="refresh" content="2;url=./">';
+}
